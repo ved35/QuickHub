6020821ee4673265b6ab87d923c748cb2961641f
Ved <vedpatel6101@gmail.com>
2025-09-26 14:05:14 +0530

    add deployment frontend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -38,10 +38,10 @@ app.use((error, req, res, next) => {
 });
 
 if(process.env.NODE_ENV === "Production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.use(express.static(path.join(__dirname,"../Frontend/dist")));
 
     app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+        res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
     });
 };
 
