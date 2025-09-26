7d31619902437a37007c1091a709d0392c71a38e
Ved <vedpatel6101@gmail.com>
2025-09-26 13:49:55 +0530

    add deployment

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 import { connectDB } from './utils/db.js';
 import userRouter from './router/user.route.js';
 import authRouter from './router/auth.route.js';
@@ -9,6 +10,7 @@ import authRouter from './router/auth.route.js';
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -34,6 +36,14 @@ app.use((error, req, res, next) => {
     message: message,
   });
 });
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    });
+};
 
 app.listen(PORT, () => {
   connectDB();
