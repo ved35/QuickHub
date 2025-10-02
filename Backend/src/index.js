a525a4c71ab2ceb78b6c94137fe8a9ed03cf4485
Ved <vedpatel6101@gmail.com>
2025-10-02 21:43:50 +0530

    Fix:backend origin cors issue

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.NODE_ENV === 'Production' ? 'https://quickhub-dtjf.onrender.com' : 'http://localhost:5173',
     credentials: true,
   })
 );
--- Frontend/src/api/axiosInstance.js
+++ Frontend/src/api/axiosInstance.js
@@ -4,7 +4,8 @@ import { showError, showWarning } from '../utils/toast';
 // const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
 
 
-const baseURL = 'https://quickhub-dtjf.onrender.com/api' 
+const baseURL = 'https://quickhub-dtjf.onrender.com/api' ;
+// const baseURL = 'http://localhost:3000/api' 
 
 // Create axios instance with base configuration
 const axiosInstance = axios.create({
