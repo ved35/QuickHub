0edcc028dbcd9c56a2fa5e73c2509b956c3b9086
Ved <vedpatel6101@gmail.com>
2025-10-02 21:55:30 +0530

    Fix:backend origin cors issue

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -21,6 +21,9 @@ app.use(
   cors({
     origin: ['https://quickhub-dtjf.onrender.com', 'http://localhost:5173'],
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    exposedHeaders: ['Set-Cookie'],
   })
 );
 
